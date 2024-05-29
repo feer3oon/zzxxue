fe3666d228d5a3b7d5e5fad636a7485d9b3829c5
feer3oon <121738576+feer3oon@users.noreply.github.com>
2024-05-29 23:50:57 +0300

    Add files via upload

---
+++ AbdoX/plugins/play/CR.py
@@ -0,0 +1,118 @@
+
+import asyncio
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AbdoX import (Apple, Resso, Spotify, Telegram, YouTube, app)
+from AbdoX import app
+from random import  choice, randint
+
+                
+@app.on_message(
+    command(["سورس","السورس"])
+    
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_video(
+        video=f"https://t.me/zzxxue/11",
+        caption=f"𝐖𝐞𝐥𝐨𝐦𝐞 𝐓𝐨 𝐒𝐨𝐮𝐫𝐜𝐞 𝙼𝚁 . 𝚇 𝐌𝐮𝐬𝐢𝐜",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/FEER3OON"), 
+                 InlineKeyboardButton(
+                   " 𝐒𝐎𝐔𝐑𝐂𝐄",       url=f"https://t.me/zzxxue"), 
+                 
+             ],[ 
+            InlineKeyboardButton(
+                        "𝙼𝚁 . 𝚇 ♟️", url=f"https://t.me/W_9_9_9"), 
+            InlineKeyboardButton(
+                        "𝗙َِ𝗘َِ𝗘َِ𝗥3َِ𝗢َِ𝗢َِ𝗡 ┇ فــ͡ـࢪعـ๋͜‏ـوُن", url=f"https://t.me/SSQ77"),       
+             ],[ 
+                  InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك⚡",
+                url=f"https://t.me/{app.username}?startgroup=true"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
+
+
+
+
+
+@app.on_message(
+    command(["مطور السورس"])
+    
+    
+)
+async def yas(client, message):
+    usr = await client.get_chat("W_9_9_9")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"معلومات مطور السورس\n\n‍ ¦dev :{name}\n\n ¦user :@{usr.username}\n\n ¦id :`{usr.id}`\n\n ¦bio :{usr.bio}\n\n 𝐒𝐎𝐔𝐑𝐂𝐄 𝗠𝗥 . 𝗫", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+
+
+@app.on_message(
+    command(["اكس" , "مستر اكس","مطور السورس"])
+    
+    
+)
+async def yas(client, message):
+    usr = await client.get_chat("W_9_9_9")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"مــعلومـات مــطور الـسـورس \n\n dev :{name}\n\n user :@{usr.username}\n\n id :`{usr.id}`\n\n boi :{usr.bio}\n\n 𝐒𝐎𝐔𝐑𝐂𝐄 𝗠𝗥 . 𝗫", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+
+
+
+@app.on_message(
+    command(["مبرمج السورس" , "فرعون","فيعون"])
+    
+    
+)
+async def yas(client, message):
+    usr = await client.get_chat("SSQ77")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"مــعلـومـات مــبرمـج الـسـورس  \n\n dev :{name}\n\n user :@{usr.username}\n\n id :`{usr.id}`\n\n bio :{usr.bio}\n\n 𝐒𝐎𝐔𝐑𝐂𝐄 𝗠𝗥 . 𝗫", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+                  )
